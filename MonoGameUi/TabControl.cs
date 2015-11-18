8ac6d6e196f1c37c7d6c88a276a696c00e15065d
Fabian Nawratil <f.nawratil@sbg.at>
2015-11-18 21:05:24 +0100

    Added VERY simple Tab Control

---
+++ MonoGameUi/TabControl.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace MonoGameUi
+{
+    public class TabControl : ContentControl
+    {
+        public ItemCollection<TabPage> Pages { get; private set; }
+
+        StackPanel tabControlStack, tabListStack;
+        ContentControl tabPage;
+
+        IScreenManager Manager;
+
+        int SelectedTabIndex = 0;
+
+        public TabControl(IScreenManager manager) : base(manager)
+        {
+            Manager = manager;
+
+            Pages = new ItemCollection<TabPage>();
+            Pages.OnInsert += OnInsert;
+            Pages.OnRemove += OnRemove;
+
+            tabControlStack = new StackPanel(manager);
+            tabControlStack.HorizontalAlignment = HorizontalAlignment.Stretch;
+            tabControlStack.VerticalAlignment = VerticalAlignment.Stretch;
+            Content = tabControlStack;
+
+            tabListStack = new StackPanel(manager);
+            tabListStack.HorizontalAlignment = HorizontalAlignment.Stretch;
+            tabListStack.Orientation = Orientation.Horizontal;
+            tabControlStack.Controls.Add(tabListStack);
+
+            tabPage = new ContentControl(manager);
+            tabPage.HorizontalAlignment = HorizontalAlignment.Stretch;
+            tabPage.VerticalAlignment = VerticalAlignment.Stretch;
+            tabPage.Background = new BorderBrush(Color.SlateGray);
+            tabControlStack.Controls.Add(tabPage);
+        }
+
+        private void OnInsert(TabPage item, int index)
+        {
+            Label title = new Label(Manager);
+            title.Text = item.Title;
+            title.Padding = Border.All(10);
+            title.Background = new BorderBrush(Color.Gray);
+            title.LeftMouseClick += (s, e) => selectTab(index);
+            tabListStack.Controls.Add(title);
+
+            selectTab(SelectedTabIndex);
+        }
+
+        private void OnRemove(TabPage item, int index)
+        {
+            tabListStack.Controls.RemoveAt(index);
+            if (Pages.Count > 0)
+                SelectedTabIndex = 0;
+                selectTab(0);
+        }
+
+        private void selectTab(int index)
+        {
+            tabListStack.Controls.ElementAt(SelectedTabIndex).Background = new BorderBrush(Color.Gray);
+            SelectedTabIndex = index;
+            tabListStack.Controls.ElementAt(index).Background = new BorderBrush(Color.LightGray);
+
+            tabPage.Content = Pages.ElementAt(SelectedTabIndex);
+        }
+
+    }
+}
---
+++ SampleClient/Screens/TabScreen.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MonoGameUi;
+
+namespace SampleClient.Screens
+{
+    class TabScreen : Screen
+    {
+        public TabScreen(IScreenManager manager) : base(manager)
+        {
+            //Create Tab Pages
+            TabPage tabPage = new TabPage(manager, "Tab 1");
+            TabPage tabPage2 = new TabPage(manager, "Tab 2");
+            TabPage tabPage3 = new TabPage(manager, "Tab 3");
+
+            //Create Tab Control & Add Pages
+            TabControl tab = new TabControl(manager);
+            tab.Pages.Add(tabPage);
+            tab.Pages.Add(tabPage2);
+            tab.Pages.Add(tabPage3);
+            tab.VerticalAlignment = VerticalAlignment.Stretch;
+            tab.HorizontalAlignment = HorizontalAlignment.Stretch;
+
+            //Add Text to Page 1
+            tabPage.Controls.Add(new Label(manager) { Text = "Content on Page 1" });
+
+            //Add "Create Tab" to page 2
+            Button createPage = Button.TextButton(manager, "Create Tab");
+            createPage.LeftMouseClick += (s, e) =>
+            {
+                TabPage page = new TabPage(manager, "NEW TAB");
+                page.Controls.Add(new Label(manager) { Text = "This is a new tab page" });
+                tab.Pages.Add(page);
+            };
+            tabPage2.Controls.Add(createPage);
+
+            //Add "Remove this page" to page 3
+            Button removePage3 = Button.TextButton(manager, "Remove this Page");
+            removePage3.LeftMouseClick += (s, e) =>
+            {
+                tab.Pages.Remove(tabPage3);
+            };
+            tabPage3.Controls.Add(removePage3);
+
+
+            Controls.Add(tab);
+        }
+    }
+}
